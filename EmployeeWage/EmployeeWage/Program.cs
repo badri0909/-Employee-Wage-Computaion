88bfdb9797fcf37ed416ac99ea433b4f67ac00fe
badri0909 <singuveerabhadrarao@gmail.com>
2022-02-26 17:53:14 +0530

    Create Attendence Method

---
+++ EmployeeWage/EmployeeWage/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EmployeeWage
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("Welcome to Employee Wage Computation");
+
+            //EmployeeAttendance.PresentorAbsent();
+
+            //EmployeeDailyWage.CalEmployee();
+
+            //EmpSwitchCase.CalEmployeeSwitch();
+
+            //EmpWagePerMonth.CalEmployeeWagePerMonth();
+
+            EmpMonthlyWageCalc.EmpWageCalcWithHoursOrDays();
+
+        }
+    }
+}
+
