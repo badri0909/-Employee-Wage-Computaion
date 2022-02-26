a09b9b517083063520ddb75aeec2708887748ff1
badri0909 <98863359+badri0909@users.noreply.github.com>
2022-02-26 17:47:54 +0530

    Add files via upload

---
+++ EmployeeAttendance.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class EmployeeAttendance
+    {
+     
+        /// UC1- the employee present or not.
+        
+        public static void PresentorAbsent()
+        {
+            Random random = new Random();
+            int r = random.Next(2);
+
+            if (r == 1)
+            {
+                Console.WriteLine("Emloyee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
